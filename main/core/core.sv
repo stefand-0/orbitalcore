0b9e8cdc0224e0128abe1946ed16dfdce4be008b
stefand-0 <stefandragan95@gmail.com>
2026-06-18 13:27:15 +0300

    Update core.sv

--- main/core/core.sv
+++ main/core/core.sv
@@ -10,8 +10,23 @@ module core(
   output logic iwswe
 );
   logic [3:0] regs [0:3];
+  typedef enum logic [1:0] {
+    FETCH = 2'b00,
+    EXECUTE = 2'b01,
+    IDLE = 2'b10
+} state_t;
+  state_t state;
   always_ff @ (posedge clk or negedge rstn) begin
     if (!rstn) begin
+      state <= FETCH;
+      iwsreg <= 4'h0;
+      iwswe <= 1'b0;
+      for(int i = 0; i < 4; i++) begin
+        regs[i] <= 4'h0;  
+      end     
+    end else begin
+      case (state)
+      endcase
     end
   end
 endmodule
