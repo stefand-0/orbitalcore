8168c47092c59045b5d0e5e763872e8a0aa690cc
stefand-0 <stefandragan95@gmail.com>
2026-06-18 13:14:21 +0300

    Add core module with clock, reset, and ports

--- main/core/core.sv
+++ main/core/core.sv
@@ -1,1 +1,17 @@
-
+module core(
+  // Clock and reset ins
+  input logic clk,
+  input logic rstn,
+  // instructions
+  output logic [15:0] imemaddr,
+  input logic [15:0] instr,
+  // iws plus periph
+  output logic [15:0] iwsreg,
+  output logic iwswe
+);
+  logic [3:0] regs [0:3];
+  always_ff @ (posedge clk or negedge rstn) begin
+    if (!rstn) begin
+    end
+  end
+endmodule
